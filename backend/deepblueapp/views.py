0fb3adff42c90eb42bdce0b2c3ae2854a553c8d2
taewoo <yun68000@naver.comgit config --global user.emaigit config --global user.email yun68000@naver.com>
2024-10-01 20:53:46 +0900

    feat: Post권한수정

--- backend/deepblueapp/views.py
+++ backend/deepblueapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import generics,permissions
+from backend.deepblueapp.models import Post
+from backend.deepblueapp.serializers import PostSerializer
 
 # Create your views here.
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    def perform_create(self,serializer):
+        serializer.save(user_profile = self.request.user.user_profile)
+    
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    def perform_update(self, serializer):
+        serializer.save(user_profile=self.request.user.user_profile)
+    
